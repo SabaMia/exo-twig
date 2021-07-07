0e8163580f5ffa99b416a834828866821a97c4a4
mia <mia.nova33@gmail.com>
2021-07-07 14:34:18 +0200

    category bdd

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,60 +3,36 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
 
-    private $categories = [
-        1 => [
-            "title" => "Politique",
-            "content" => "Tous les articles liés à Jean Lassalle",
-            "id" => 1,
-            "published" => true,
-        ],
-        2 => [
-            "title" => "Economie",
-            "content" => "Les meilleurs tuyaux pour avoir DU FRIC",
-            "id" => 2,
-            "published" => true
-        ],
-        3 => [
-            "title" => "Securité",
-            "content" => "Attention les étrangers sont très méchants",
-            "id" => 3,
-            "published" => false
-        ],
-        4 => [
-            "title" => "Ecologie",
-            "content" => "Hummer <3",
-            "id" => 4,
-            "published" => true
-        ]
-    ];
-
-
     /**
-     * @Route ("/", name="listCategories")
+     * @Route ("/categories", name="listCategories")
      * création de la première page avec sa route et la fonction qui renvoie au fichier twig correspondant
      */
-    public function listCategories()
+    public function listCategories(CategoryRepository $categoryRepository)
     {
+        $categories = $categoryRepository->findAll();
         return $this->render('list-categories.html.twig',[
-            'categories' => $this-> categories
+            'categories' => $categories
         ]);
     }
 
     /**
-     * @Route ("/categorie/{id}", name="showCategories")
+     * @Route ("/category/{id}", name="showCategories")
      * création de la deuxième page avec sa route et la fonction qui renvoie au fichier twig correspondant
      * on insère l'id à chaque fois que le contenu doit correspondre à celui de l'article sélectionné
      */
-    public function showCategories($id)
+    public function showCategories($id, CategoryRepository $categoryRepository)
     {
+        // afficher l'article avec à partir de son id (wilcard dans l'url))
+        $category = $categoryRepository->find($id);
         return $this->render('show-categorie.html.twig',[
-                'categorie' => $this-> categories[$id]
+                'category' => $category
         ]);
     }
 
